a40751245a42f733655422da06f48f9cb408e72a
Ningyue Ji <79038651+738NGX@users.noreply.github.com>
2023-03-30 23:13:45 +0800

    Console Applications

---
+++ src/msfs/msfs.cpp
@@ -0,0 +1,19 @@
+﻿#include <windows.h>
+#include <tchar.h>
+#include <iostream>
+#include <strsafe.h>
+
+#include "..\..\inc\SimConnect.h"
+#include "..\..\inc\PMDG_NG3_SDK.h"
+#include "..\..\inc\Noobfish.h"
+
+#pragma comment(lib, "..\\..\\lib\\MSFS\\SimConnect.lib")
+
+bool requestCDUData();
+
+int main() {
+	requestCDUData();
+	NG3_run();
+	return 0;
+}
+
---
+++ src/p4d/p4d.cpp
@@ -0,0 +1,19 @@
+﻿#include <windows.h>
+#include <tchar.h>
+#include <iostream>
+#include <strsafe.h>
+
+#include "..\..\inc\SimConnect.h"
+#include "..\..\inc\PMDG_NG3_SDK.h"
+#include "..\..\inc\Noobfish.h"
+
+#pragma comment(lib, "..\\..\\lib\\P4D\\SimConnect.lib")
+
+int main() {
+	NG3_run();
+}
+
+
+
+
+
